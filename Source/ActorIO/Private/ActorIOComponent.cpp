e8ae0ef85377e8b34e538fceeee9ae1835d0d574
HorizonGamesRoland <horizongames.contact@gmail.com>
2024-10-20 17:29:55 +0200

    Actor IO component framework

--- Source/ActorIO/Private/ActorIOComponent.cpp
+++ Source/ActorIO/Private/ActorIOComponent.cpp
@@ -1,3 +1,63 @@
 // Copyright 2024 Horizon Games. All Rights Reserved.
 
 #include "ActorIOComponent.h"
+
+void UActorIOComponent::OnRegister()
+{
+	Super::OnRegister();
+}
+
+void UActorIOComponent::AddAction(const FActorIOAction& NewAction)
+{
+	if (!NewAction.IsValid())
+	{
+		UE_LOG(LogTemp, Error, TEXT("ActorIOComponent: AddAction failed - The action was invalid!"));
+		return;
+	}
+
+	if (HasAction(NewAction.ActionId))
+	{
+		UE_LOG(LogTemp, Error, TEXT("ActorIOComponent: AddAction failed - The action already exists!"));
+		return;
+	}
+
+
+}
+
+void UActorIOComponent::RemoveAction(const FGuid& ActionId)
+{
+	
+}
+
+bool UActorIOComponent::GetAction(const FGuid& ActionId, FActorIOAction& OutAction)
+{
+	return false;
+}
+
+bool UActorIOComponent::HasAction(const FGuid& ActionId) const
+{
+	for (const FActorIOAction& Action : ActionBindings)
+	{
+		if (Action.ActionId == ActionId)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void UActorIOComponent::BindActions()
+{
+
+}
+
+void UActorIOComponent::RemoveActionBindings()
+{
+
+}
+
+void UActorIOComponent::OnUnregister()
+{
+	Super::OnUnregister();
+}
--- Source/ActorIO/Public/ActorIOComponent.h
+++ Source/ActorIO/Public/ActorIOComponent.h
@@ -10,10 +10,63 @@ USTRUCT()
 struct FActorIOAction
 {
 	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Properties")
+	FGuid ActionId;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Properties")
+	FName SourceEvent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Properties")
+	TObjectPtr<AActor> TargetActor;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Properties")
+	FName TargetFunction;
+
+	FDelegateHandle ActionDelegateHandle;
+
+	FActorIOAction() :
+		ActionId(FGuid::NewGuid()),
+		SourceEvent(FName()),
+		TargetActor(nullptr),
+		TargetFunction(FName()),
+		ActionDelegateHandle(FDelegateHandle())
+	{}
+
+	bool IsValid() const
+	{
+		return ActionId.IsValid() && !SourceEvent.IsNone() && TargetActor && !TargetFunction.IsNone();
+	}
 };
 
 UCLASS()
 class ACTORIO_API UActorIOComponent : public UActorComponent
 {
 	GENERATED_BODY()
+
+private:
+
+	UPROPERTY()
+	TArray<FActorIOAction> ActionBindings;
+
+public:
+
+	void AddAction(const FActorIOAction& NewAction);
+
+	void RemoveAction(const FGuid& ActionId);
+
+	bool GetAction(const FGuid& ActionId, FActorIOAction& OutAction);
+
+	bool HasAction(const FGuid& ActionId) const;
+
+protected:
+
+	void BindActions();
+
+	void RemoveActionBindings();
+
+public:
+
+	virtual void OnRegister() override;
+	virtual void OnUnregister() override;
 };
