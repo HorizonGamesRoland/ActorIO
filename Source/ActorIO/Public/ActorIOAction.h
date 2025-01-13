6ed77a896cea118f1a9fcc44820beec8f1f66418
HorizonGamesRoland <horizongames.contact@gmail.com>
2025-01-13 18:28:25 +0100

    Actor IO action and component code documentation

--- Source/ActorIO/Public/ActorIOAction.h
+++ Source/ActorIO/Public/ActorIOAction.h
@@ -8,6 +8,11 @@
 
 class UActorIOComponent;
 
+/**
+ * An action that will be execute when the assigned I/O event is triggered.
+ * When executed, it will call the designated function on the target actor with the given parameters.
+ * This is basically the message bus of the I/O system.
+ */
 UCLASS(DefaultToInstanced, EditInlineNew)
 class ACTORIO_API UActorIOAction : public UObject
 {
@@ -15,60 +20,99 @@ class ACTORIO_API UActorIOAction : public UObject
 
 public:
 
+	/** Default constructor. */
 	UActorIOAction();
 
 public:
 
+	/** Id of the I/O event to bind to. */
 	UPROPERTY(EditAnywhere, Category = "Action")
 	FName EventId;
 
+	/** The execution target. The action will call the designated function on this actor. */
 	UPROPERTY(EditAnywhere, Category = "Action")
 	TObjectPtr<AActor> TargetActor;
 
+	/** Id of the I/O function to execute on the target actor. */
 	UPROPERTY(EditAnywhere, Category = "Action")
 	FName FunctionId;
 
+	/**
+	 * Parameters that are sent with the function.
+	 * Format is: Arg1 Arg2 Arg3 ...
+	 * Also supports named arguments in the form of $ArgName.
+	 */
 	UPROPERTY(EditAnywhere, Category = "Action")
 	FString FunctionArguments;
 
+	/** Time before the function is called on the target actor, after the action is executed. */
 	UPROPERTY(EditAnywhere, Category = "Action")
 	float Delay;
 
+	/**
+	 * Whether the action can only be executed once.
+	 * If false, the action will be executed every time the assigned I/O event is triggered.
+	 */
 	UPROPERTY(EditAnywhere, Category = "Action")
 	bool bExecuteOnlyOnce;
 
 protected:
 
+	/** Whether the action was executed before. */
 	bool bWasExecuted;
 
+	/** Whether the action is bound to the assigned I/O event. */
 	bool bIsBound;
 
+	/** The delegate that is bound to the assigned I/O event. */
 	FScriptDelegate ActionDelegate;
 
 public:
 
+	/** Bind the action to the assigned I/O event. */
 	void BindAction();
 
+	/** Unbind the action. */
 	void UnbindAction();
 
+	/** @return The I/O component that owns this action. */
 	UActorIOComponent* GetOwnerIOComponent() const;
 
+	/** @return The parent actor of the owning I/O component. */
 	AActor* GetOwnerActor() const;
 
 protected:
 
+	/**
+	 * Name of the function that is bound to the assigned I/O event.
+	 * This function will be used as a notify so that we can keep track of the global action execution state.
+	 * By default this is assigned to the "ReceiveExecuteAction" function below.
+	 */
 	static FName ExecuteActionSignalName;
 
+	/**
+	 * Entry point when the assigned I/O event is triggered.
+	 * This function does not do anything on its own.
+	 * It is simply used as a notify so that we can keep track of the global action execution state.
+	 */
 	UFUNCTION()
 	void ReceiveExecuteAction();
 
+	/** @return Whether the action can be executed or not. */
 	bool CanExecuteAction(FActionExecutionContext& ExecutionContext);
 
+	/** Executes the action. */
 	void ExecuteAction(FActionExecutionContext& ExecutionContext);
 
+	/**
+	 * Sends the final command to the target actor.
+	 * The command contains the function name and parameter list that will be processed by Unreal Script.
+	 */
 	void SendCommand(UObject* TargetObject, FString Command);
 
 public:
 
+	//~ Begin UObject Interface
 	virtual void ProcessEvent(UFunction* Function, void* Parms) override;
+	//~ End UObject Interface
 };
--- Source/ActorIO/Public/ActorIOComponent.h
+++ Source/ActorIO/Public/ActorIOComponent.h
@@ -8,6 +8,10 @@
 
 class UActorIOAction;
 
+/**
+ * The component that manages I/O actions for the actor.
+ * Acts as an interface for the I/O system.
+ */
 UCLASS(Blueprintable, ClassGroup = "Actor IO")
 class ACTORIO_API UActorIOComponent : public UActorComponent
 {
@@ -15,35 +19,47 @@ class ACTORIO_API UActorIOComponent : public UActorComponent
 
 public:
 
+	/** Default constructor. */
 	UActorIOComponent();
 
 protected:
 
-	UPROPERTY(Instanced, VisibleInstanceOnly)
+	/** List of actions tied to the component. */
+	UPROPERTY(Instanced, EditInstanceOnly)
 	TArray<TObjectPtr<UActorIOAction>> Actions;
 
 public:
 
+	/** Creates a new I/O action and adds it to the action list. */
 	UActorIOAction* CreateNewAction();
 
+	/** Removes the given action from the action list, and destroys it. */
 	void RemoveAction(UActorIOAction* InAction);
 
+	/** Removes all entries from the action list that are nullptr. */
 	void RemoveInvalidActions();
 
+	/** @return Weak reference to all I/O actions. */
 	TArray<TWeakObjectPtr<UActorIOAction>> GetActions() const;
 
+	/** @return Number of actions. */
 	int32 GetNumActions() const { return Actions.Num(); }
 
+	/** @return Delay of the longest delayed action. */
 	float GetDurationOfLongestDelay() const;
 
 protected:
 
+	/** Binds all actions to their assigned I/O events. */
 	void BindActions();
 
+	/** Unbinds all actions. */
 	void UnbindActions();
 
 public:
 
+	//~ Begin UActorComponent Interface
 	virtual void OnRegister() override;
 	virtual void OnUnregister() override;
+	//~ End UActorComponent Interface
 };
